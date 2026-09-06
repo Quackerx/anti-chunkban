0b1961d2c24551116f1ff0d529dbe1910b45b7db
Quackerx <nostrenghtnoduck@gmail.com>
2026-09-06 20:58:12 +0300

    Delete src/main/src/main directory

--- src/main/src/main/java/anti/ban/AntiChunkban.java
+++
@@ -1,9 +0,0 @@
-package anti.ban;
-
-import net.fabricmc.api.ModInitializer;
-
-public class AntiChunkban implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
--- src/main/src/main/java/anti/ban/Saver.java
+++
@@ -1,7 +0,0 @@
-package anti.ban;
-
-public class Saver {
-    public static boolean signEnabled;
-    public static boolean dropsEnabled;
-    public static boolean chunkban;
-}
